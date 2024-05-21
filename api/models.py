f67cadb59a9e041cd4527d82160e00f4b1209142
Nguyen Gia Huy <74499773+hyu758@users.noreply.github.com>
2024-05-21 21:44:09 +0700

    Add files via upload

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class products(models.Model):
+    search_image = models.TextField(default='')
+    styleid = models.CharField(max_length=30)
+    brand_filter_facet = models.CharField(max_length=30)
+    price = models.IntegerField(default=0)
+    product_addtional_info = models.CharField(max_length=100)
