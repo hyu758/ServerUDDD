999a6d0489f02d20d05ef2ee2210cdec42ea873f
hyu758 <huy20041512@gmail.com>
2024-05-22 21:21:00 +0700

    fix

--- api/models.py
+++ api/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.db import models
 
 # Create your models here.
@@ -14,21 +13,4 @@ class accounts(models.Model):
     firstName = models.CharField(max_length=20, default="")
     lastName = models.CharField(max_length=20, default="")
     email = models.EmailField(unique=True, default="")
-=======
-from django.db import models
-
-# Create your models here.
-class products(models.Model):
-    image = models.TextField(default='')
-    productName = models.CharField(max_length=50)
-    price = models.FloatField(default=0)
-    brand = models.CharField(max_length=50)
-    yearOfManufacture = models.IntegerField(default=0)
-    description = models.TextField(default='')
-
-class accounts(models.Model):
-    firstName = models.CharField(max_length=20, default="")
-    lastName = models.CharField(max_length=20, default="")
-    email = models.EmailField(unique=True, default="")
->>>>>>> master
     password = models.CharField(max_length=20)
